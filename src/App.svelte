2037b6cd8ffcc2417fbf4c8716b3740760b8ad15
Nara <elesrubsoap_4822@hotmail.com>
2021-06-24 21:19:08 +0700

    :construction: WIP: add MeetupItem Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,39 @@
 <script>
   import Header from "./UI/Header.svelte";
+  import MeetingItem from "./Meetups/MeetingItem.svelte";
+
+  const meetups = [
+    {
+      id: "m1",
+      title: "Coding BootCamp 1",
+      subtitle: "Learn to code 1",
+      description: "In ...",
+      imageUrl: "",
+      address: "some address 1",
+      contactEmail: "some email 1",
+    },
+    {
+      id: "m2",
+      title: "Coding BootCamp 2",
+      subtitle: "Learn to code 2",
+      description: "In ...",
+      imageUrl: "",
+      address: "some address 2",
+      contactEmail: "some email 2",
+    },
+  ];
 </script>
 
 <Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetingItem />
+  {/each}
+</section>
+
+<style>
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
