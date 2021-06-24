0a8f9aebf22e1037f2907374cb8c25ca18772541
Nara <elesrubsoap_4822@hotmail.com>
2021-06-24 21:47:37 +0700

    :construction: style: add MeetUpGrid Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Header from "./UI/Header.svelte";
-  import MeetingItem from "./Meetups/MeetingItem.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
 
   const meetups = [
     {
@@ -8,7 +8,8 @@
       title: "Coding BootCamp 1",
       subtitle: "Learn to code 1",
       description: "In ...",
-      imageUrl: "",
+      imageUrl:
+        "https://images.unsplash.com/photo-1614531341773-3bff8b7cb3fc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
       address: "some address 1",
       contactEmail: "some email 1",
     },
@@ -17,7 +18,8 @@
       title: "Coding BootCamp 2",
       subtitle: "Learn to code 2",
       description: "In ...",
-      imageUrl: "",
+      imageUrl:
+        "https://images.unsplash.com/photo-1611329532992-0b7ba27d85fb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
       address: "some address 2",
       contactEmail: "some email 2",
     },
@@ -26,14 +28,9 @@
 
 <Header />
 
-<section id="meetups">
-  {#each meetups as meetup}
-    <MeetingItem />
-  {/each}
-</section>
+<main>
+  <MeetupGrid {meetups} />
+</main>
 
 <style>
-  #meetups {
-    margin-top: 5rem;
-  }
 </style>
---
+++ src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,37 @@
+<script>
+  import MeetingItem from "./MeetingItem.svelte";
+
+  export let meetups;
+</script>
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetingItem
+      title={meetup.title}
+      subtitle={meetup.subtitle}
+      description={meetup.description}
+      address={meetup.address}
+      imageUrl={meetup.imageUrl}
+      email={meetup.email}
+    />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  #meetups {
+    margin-top: 5rem;
+  }
+</style>
